23e501cfd9ba36bdf932f68b882b554e8ca66e58
Dave Garwacke <dave.garwacke@warbyparker.com>
2018-03-15 15:53:23 -0400

    chore() Remember this stuff

---
+++ demo/components/Article.jsx
@@ -0,0 +1,16 @@
+import d from "../../src/d";
+
+export default ({ title, body, id, fetchPost }) => {
+  return (
+    <article
+      style={{
+        border: "1px black solid",
+        padding: "12px 18px",
+      }}
+    >
+      <h1>{title}</h1>
+      <p>{body}</p>
+      {fetchPost && <button onClick={() => fetchPost(id)}>Read more</button>}
+    </article>
+  );
+};
