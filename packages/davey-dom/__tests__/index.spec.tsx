1c65620de752b7a754f303780996fe2778998c0f
Dave Garwacke <admin+git@ifyoumakeit.com>
2018-03-25 08:51:33 -0400

    test(*): Set up tests for all packages

--- packages/davey-dom/__tests__/index.spec.tsx
+++ packages/davey-dom/__tests__/index.spec.tsx
@@ -1,27 +1,17 @@
 import { davey } from "davey";
+import { render } from "davey-dom";
+import { JSDOM } from "jsdom";
 
 describe("Basic single element", () => {
+  const dom = new JSDOM(
+    `<!DOCTYPE html><html><body><div id="root"></div></body></html>`
+  );
   const Component = <h1>hello</h1>;
+  console.log(dom.window.document.querySelector("#root"));
   test("match snapshot", () => {
-    expect(Component).toMatchSnapshot();
-  });
-  test("returns tuple", () => {
-    expect(Array.isArray(Component)).toBe(true);
-  });
-  test("first element of tuple is tag", () => {
-    expect(Component[0]).toBe("h1");
-  });
-  test("second element of tuple is props object", () => {
-    expect(typeof Component[1]).toBe("object");
-  });
-  test("second element of tuple has children", () => {
-    expect(Component[1]).toHaveProperty("children");
-  });
-  test("children are an array", () => {
-    expect(Array.isArray(Component[1].children)).toBe(true);
-  });
-  test("children contain correct text string", () => {
-    expect(Component[1].children[0]).toBe("hello");
+    expect(
+      render(Component, dom.window.document.querySelector("#root"))
+    ).toMatchSnapshot();
   });
 });
 
@@ -56,7 +46,11 @@ describe("2-level deep nested element", () => {
     expect(Component[1].children[0][0]).toBe("header");
   });
   test("hello world be present", () => {
-    expect(Component[1].children[0][1].children[0][1].children[0]).toBe("hello");
-    expect(Component[1].children[0][1].children[1][1].children[0]).toBe("world");
+    expect(Component[1].children[0][1].children[0][1].children[0]).toBe(
+      "hello"
+    );
+    expect(Component[1].children[0][1].children[1][1].children[0]).toBe(
+      "world"
+    );
   });
 });
