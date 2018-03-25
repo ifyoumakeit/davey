077364a33043620b545ae9a06ebe8da412bad171
Dave Garwacke <admin+git@ifyoumakeit.com>
2018-03-25 18:58:33 -0400

    test(davey): Add function resolution

--- packages/davey/__tests__/index.spec.tsx
+++ packages/davey/__tests__/index.spec.tsx
@@ -1,27 +1,34 @@
 import { davey } from "davey";
 
 describe("Basic single element", () => {
-  const Component = <h1>hello</h1>;
-  test("match snapshot", () => {
-    expect(Component).toMatchSnapshot();
+  const ComponentJsx = <h1>hello</h1>;
+  const ComponentFn = davey("h1", null, "hello");
+
+  test("jsx === function", () => {
+    expect(ComponentJsx).toEqual(ComponentFn);
   });
-  test("returns tuple", () => {
-    expect(Array.isArray(Component)).toBe(true);
-  });
-  test("first element of tuple is tag", () => {
-    expect(Component[0]).toBe("h1");
-  });
-  test("second element of tuple is props object", () => {
-    expect(typeof Component[1]).toBe("object");
-  });
-  test("second element of tuple has children", () => {
-    expect(Component[1]).toHaveProperty("children");
-  });
-  test("children are an array", () => {
-    expect(Array.isArray(Component[1].children)).toBe(true);
-  });
-  test("children contain correct text string", () => {
-    expect(Component[1].children[0]).toBe("hello");
+  [ComponentJsx, ComponentFn].forEach(Component => {
+    test("match snapshot", () => {
+      expect(Component).toMatchSnapshot();
+    });
+    test("returns tuple", () => {
+      expect(Array.isArray(Component)).toBe(true);
+    });
+    test("first element of tuple is tag", () => {
+      expect(Component[0]).toBe("h1");
+    });
+    test("second element of tuple is props object", () => {
+      expect(typeof Component[1]).toBe("object");
+    });
+    test("second element of tuple has children", () => {
+      expect(Component[1]).toHaveProperty("children");
+    });
+    test("children are an array", () => {
+      expect(Array.isArray(Component[1].children)).toBe(true);
+    });
+    test("children contain correct text string", () => {
+      expect(Component[1].children[0]).toBe("hello");
+    });
   });
 });
 
@@ -56,7 +63,11 @@ describe("2-level deep nested element", () => {
     expect(Component[1].children[0][0]).toBe("header");
   });
   test("hello world be present", () => {
-    expect(Component[1].children[0][1].children[0][1].children[0]).toBe("hello");
-    expect(Component[1].children[0][1].children[1][1].children[0]).toBe("world");
+    expect(Component[1].children[0][1].children[0][1].children[0]).toBe(
+      "hello"
+    );
+    expect(Component[1].children[0][1].children[1][1].children[0]).toBe(
+      "world"
+    );
   });
 });
