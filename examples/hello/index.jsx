00303cc600116ee48335fc891c527a38cd4015ba
Dave Garwacke <admin+git@ifyoumakeit.com>
2018-03-27 08:55:03 -0400

    fix(demo): Remove cray

--- examples/hello/index.jsx
+++ examples/hello/index.jsx
@@ -1,4 +1,4 @@
 import { davey } from "davey";
 import { render } from "davey-dom";
 
-render(<h1>Hello <span>cray</span> world!</h1>, document.querySelector("#root"));
+render(<h1>Hello world!</h1>, document.querySelector("#root"));
