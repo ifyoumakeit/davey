920e46a2b82b3526fcc442fffb429169b73e839c
Dave Garwacke <admin+git@ifyoumakeit.com>
2018-05-27 18:20:10 -0400

    feat: canary release

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -2,11 +2,12 @@
 set -e
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "We are in a pull request, not releasing..."
-  yarn lerna publish --skip-npm --skip-git --conventional-commits --yes
+  echo "We are in a pull request, releasing canary..."
+  yarn lerna publish --canary --conventional-commits --yes
   exit 0
 fi
 
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
+  echo "Releasing from master.."
   yarn lerna publish --conventional-commits --yes
 fi
