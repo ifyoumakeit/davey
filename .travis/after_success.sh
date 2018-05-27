7a43b43eb9ccda36d160100342829c795875f40e
Dave Garwacke <admin+git@ifyoumakeit.com>
2018-05-27 17:17:07 -0400

    fix: add post_success script

---
+++ .travis/after_success.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  yarn lerna publish --skip-npm --skip-git --conventional-commits --yes
+  exit 0
+fi
+
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
+  yarn lerna publish --conventional-commits --yes
+fi
