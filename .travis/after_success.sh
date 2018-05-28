a0dc68394714dcdbe7c4a5b652c7534d353e7c9a
Dave Garwacke <admin+git@ifyoumakeit.com>
2018-05-28 01:43:55 -0400

    refactor: who cares

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -3,12 +3,11 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo "We are in a pull request, releasing canary..."
-  yarn lerna-semantic-release perform --canary
+  yarn lerna publish --canary --conventional-commits --yes
   exit 0
 fi
 
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
   echo "Releasing from master.."  
-  yarn lerna-semantic-release perform
-  yarn lerna-semantic-release post
+  yarn lerna publish --conventional-commits --yes --changelog-preset conventional
 fi
